b63b68bd3434e9967c233c58bb2a7d66e6bdb2a5
Dagiayy <ayenewdagmawi@gmail.com>
2025-07-11 05:45:37 -0700

    Store raw Telegram messages in partitioned data_lake/raw/telegram_messages/YYYY-MM-DD/channel.json

--- app/scraper/main.py
+++ app/scraper/main.py
@@ -3,5 +3,5 @@ import os
 
 load_dotenv()
 
-api_id = os.getenv("TELEGRAM_API_ID")
-api_hash = os.getenv("TELEGRAM_API_HASH")
+api_id = os.getenv("25137965")
+api_hash = os.getenv("5b75b63d388dc496776c90b42da6f4e6")
